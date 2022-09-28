82119cb87c137bef9435b3d182caab88c40a6812
tkgstrator <nasawake.am@gmail.com>
2022-09-28 11:33:04 +0900

    fix: foreground color in golden eggs appearances

--- Salmonia3+/Components/Results/Components/ResultWave.swift
+++ Salmonia3+/Components/Results/Components/ResultWave.swift
@@ -14,49 +14,52 @@ struct ResultWave: View {
     var body: some View {
         GeometryReader(content: { geometry in
             let scale: CGFloat = geometry.width / 124
-            VStack(alignment: .center, spacing: 0, content: {
-                Text("Wave \(wave.id)")
-                    .font(systemName: .Splatfont2, size: 17 * scale)
-                    .frame(height: 25 * scale, alignment: .center)
-                    .foregroundColor(.black)
-                ZStack(content: {
-                    Rectangle().fill(SPColor.Theme.SPDark)
-                    if let goldenIkuraNum = wave.goldenIkuraNum, let quotaNum = wave.quotaNum {
-                        Text("\(goldenIkuraNum)/\(quotaNum)")
-                            .font(systemName: .Splatfont2, size: 25 * scale)
-                            .frame(height: 36.5 * scale, alignment: .center)
-                            .foregroundColor(.white)
-                    } else {
-                        Text(SakelienType.SakelienGiant.localizedText)
-                            .font(systemName: .Splatfont2, size: 25 * scale)
-                            .frame(height: 36.5 * scale, alignment: .center)
-                            .foregroundColor(.red)
-                    }
-                })
-                .padding(.top, 2 * scale)
-                .frame(height: 36.5 * scale, alignment: .center)
-                Text(wave.waterLevel.localizedText)
-                    .font(systemName: .Splatfont2, size: 16 * scale)
-                    .foregroundColor(.black)
-                    .padding(.top, 8 * scale)
-                Text(wave.eventType.localizedText)
-                    .font(systemName: .Splatfont2, size: 16 * scale)
-                    .foregroundColor(.black)
-                HStack(spacing: nil, content: {
-                    Image(bundle: .Golden)
-                        .resizable()
-                        .frame(width: 20 * scale, height: 20 * scale, alignment: .center)
-                    Text("x\(wave.goldenIkuraPopNum)")
+            ZStack(content: {
+                RoundedRectangle(cornerRadius: 6).fill(SPColor.Theme.SPYellow)
+                    .aspectRatio(124/180, contentMode: .fit)
+                    .overlay(WaterLevel().fill(Color.black.opacity(0.2)).offset(x: 0, y: wave.waterLevel.height * scale).clipped())
+                    .mask(Hanger().scaledToFill())
+                VStack(alignment: .center, spacing: 0, content: {
+                    Text("Wave \(wave.id)")
+                        .font(systemName: .Splatfont2, size: 17 * scale)
+                        .frame(height: 25 * scale, alignment: .center)
+                        .foregroundColor(.black)
+                    ZStack(content: {
+                        Rectangle().fill(SPColor.Theme.SPDark)
+                        if let goldenIkuraNum = wave.goldenIkuraNum, let quotaNum = wave.quotaNum {
+                            Text("\(goldenIkuraNum)/\(quotaNum)")
+                                .font(systemName: .Splatfont2, size: 25 * scale)
+                                .frame(height: 36.5 * scale, alignment: .center)
+                                .foregroundColor(.white)
+                        } else {
+                            Text(SakelienType.SakelienGiant.localizedText)
+                                .font(systemName: .Splatfont2, size: 25 * scale)
+                                .frame(height: 36.5 * scale, alignment: .center)
+                                .foregroundColor(.red)
+                        }
+                    })
+                    .padding(.top, 2 * scale)
+                    .frame(height: 36.5 * scale, alignment: .center)
+                    Text(wave.waterLevel.localizedText)
                         .font(systemName: .Splatfont2, size: 16 * scale)
-                        .foregroundColor(.gray)
+                        .foregroundColor(.black)
+                        .padding(.top, 8 * scale)
+                    Text(wave.eventType.localizedText)
+                        .font(systemName: .Splatfont2, size: 16 * scale)
+                        .foregroundColor(.black)
+                    HStack(spacing: nil, content: {
+                        Image(bundle: .Golden)
+                            .resizable()
+                            .frame(width: 20 * scale, height: 20 * scale, alignment: .center)
+                        Text("x\(wave.goldenIkuraPopNum)")
+                            .font(systemName: .Splatfont2, size: 16 * scale)
+                            .foregroundColor(.white)
+                            .shadow(color: .black, radius: 0, x: 1, y: 1)
+                    })
                 })
+                .frame(maxWidth: .infinity)
             })
-            .frame(maxWidth: .infinity)
-            .padding(.vertical, 16 * scale)
-            .overlay(WaterLevel().fill(Color.black.opacity(0.2)).offset(x: 0, y: wave.waterLevel.height * scale).clipped())
         })
-        .background(RoundedRectangle(cornerRadius: 6).fill(SPColor.Theme.SPYellow))
-        .mask(Hanger().scaledToFill())
         .aspectRatio(124/180, contentMode: .fit)
     }
 }
